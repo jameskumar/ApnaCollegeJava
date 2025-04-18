f436077148b6314b1d8b9d24d68003290d25305d
Jemas Kumar <James.sonam1@gmail.com>
2025-04-18 20:49:06 +0530

    output!

---
+++ src/com/apnacollege/Output.java
@@ -0,0 +1,55 @@
+package com.apnacollege;
+
+public class Output {
+    public static void main(String[] args) {
+        // output
+        // print without next line with print
+        System.out.print("Hello World with Java");
+        System.out.print("Hello World with Java");
+
+        System.out.print("\n");
+
+        // print with next line with println
+        System.out.println("Hello World with Java");
+        System.out.println("Hello World with Java");
+
+        System.out.print("\n");
+
+        // print without next line with print and put next line with \n
+        System.out.print("Hello World with Java\n");
+        System.out.print("Hello World with Java\n");
+
+        System.out.println("\n");
+
+        System.out.print("Hello World with Java\nfrom\n Apni Kaksha");
+
+        /*
+        * print the pattern in simple way
+        * *
+        * **
+        * ***
+        * ****
+        * */
+
+        System.out.println("\n");
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+        System.out.println("\n");
+
+        /*
+         * print the pattern
+         * *
+         * **
+         */
+
+        System.out.println("*\n**");
+
+        System.out.println("\n");
+
+
+    }
+}
