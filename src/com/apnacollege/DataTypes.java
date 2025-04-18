3805721c8fe9943c63c81a9622d539047b40e7fa
Jemas Kumar <James.sonam1@gmail.com>
2025-04-18 22:48:51 +0530

    DataTypes!

---
+++ src/com/apnacollege/DataTypes.java
@@ -0,0 +1,33 @@
+package com.apnacollege;
+
+public class DataTypes {
+
+    public static void main(String[] args) {
+
+        // Data Types
+        int a = 10;
+        int b = 25;
+
+        // Sum
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+
+        // Diff
+        int diff = b - a;
+        System.out.println("Diff of " + b + " and " + a + " is: " + diff);
+
+        // Multiplication
+        int mul = a * b;
+        System.out.println("Multiplication of " + a + " and " + b + " is: " + mul);
+
+        byte byte_var = 0x10; // 1 byte data
+        System.out.println("Byte value is (Decimal): " + byte_var);
+        System.out.println("Byte value is (Hex): 0x" + Integer.toHexString(byte_var & 0xFF));       // Masking to get the last 8 bits and as a positive value
+        System.out.println("Byte value is (Binary): " + Integer.toBinaryString(byte_var & 0xFF));   // Masking to get the last 8 bits and as a positive value
+
+        // quiz: calculate a x b / a - b for a = 10, b = 5
+        a = 10;
+        b = 5;
+        System.out.println("Quiz answer: " + ((a * b) / (a - b)) );
+    }
+}
---
+++ src/com/apnacollege/Variable.java
@@ -0,0 +1,19 @@
+package com.apnacollege;
+
+public class Variable {
+
+    public static void main(String[] args) {
+
+        // Variables
+        int a = 25;
+        int b = 10;
+
+        String name = "Tony Stack";
+        int age = 48;
+
+        double price = 25.25;
+
+        b = 20;
+        name = "ironman";
+    }
+}
