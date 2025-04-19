e925e929e1873834eb47921ab7b305de6b827078
Jemas Kumar <James.sonam1@gmail.com>
2025-04-20 03:05:42 +0530

    Arrays!

---
+++ src/com/apnacollege/Arrays.java
@@ -0,0 +1,188 @@
+package com.apnacollege;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        // Arrays are a collection of similar type of data
+        // that are stored in a single variable.
+        // It's a non-primitive data type.
+
+        // Declare an array
+        // type[] arrayName = new type[size];
+        // int[] marks = new int[3];
+
+        // Index starts from 0.
+        // marks[0] = 10;
+        // marks[1] = 20;
+        // marks[2] = 30;
+
+        // int[] marks = {10, 20, 30};
+
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        // for (int i = 0; i < marks.length; i++) {
+        //     System.out.println(marks[i]);
+        // }
+
+        // for (int mark : marks) {
+        //     System.out.println(mark);
+        // }
+
+        // int[][] marks = {
+        //     {10, 20, 30},     // 0
+        //     {40, 50, 60},     // 1
+        //     {70, 80, 90}      // 2
+        // };
+
+        // System.out.println(marks[0][0]);
+        // System.out.println(marks[0][1]);
+        // System.out.println(marks[0][2]);
+
+        // System.out.println(marks[1][0]);
+        // System.out.println(marks[1][1]);
+        // System.out.println(marks[1][2]);
+
+        // System.out.println(marks[2][0]);
+        // System.out.println(marks[2][1]);
+        // System.out.println(marks[2][2]);
+
+        // for (int i = 0; i < marks.length; i++) {
+        //     for (int j = 0; j < marks[i].length; j++) {
+        //         System.out.println(marks[i][j]);
+        //     }
+        // }
+
+        // for (int[] mark : marks) {
+        //     for (int mark1 : mark) {
+        //         System.out.println(mark1);
+        //     }
+        // }
+
+        // int[][] marks = new int[3][3];
+        // marks[0][0] = 10;
+        // marks[0][1] = 20;
+        // marks[0][2] = 30;
+
+        // marks[1][0] = 40;
+        // marks[1][1] = 50;
+        // marks[1][2] = 60;
+
+        // marks[2][0] = 70;
+        // marks[2][1] = 80;
+        // marks[2][2] = 90;
+
+        // System.out.println(marks[0][0]);
+        // System.out.println(marks[0][1]);
+        // System.out.println(marks[0][2]);
+
+        // System.out.println(marks[1][0]);
+        // System.out.println(marks[1][1]);
+        // System.out.println(marks[1][2]);
+
+        // System.out.println(marks[2][0]);
+        // System.out.println(marks[2][1]);
+        // System.out.println(marks[2][2]);
+
+        // for (int i = 0; i < marks.length; i++) {
+        //     for (int j = 0; j < marks[i].length; j++) {
+        //         System.out.println(marks[i][j]);
+        //     }
+        // }
+
+        // for (int[] mark : marks) {
+        //     for (int mark1 : mark) {
+        //         System.out.println(mark1);
+        //     }
+        // }
+
+        // int[][] marks = new int[3][];
+        // marks[0] = new int[3];
+        // marks[1] = new int[2];
+        // marks[2] = new int[1];
+
+        // marks[0][0] = 10;
+        // marks[0][1] = 20;
+        // marks[0][2] = 30;
+
+        // marks[1][0] = 40;
+        // marks[1][1] = 50;
+
+        // marks[2][0] = 70;
+
+        // System.out.println(marks[0][0]);
+        // System.out.println(marks[0][1]);
+        // System.out.println(marks[0][2]);
+
+        // System.out.println(marks[1][0]);
+        // System.out.println(marks[1][1]);
+
+        // System.out.println(marks[2][0]);
+
+        // for (int i = 0; i < marks.length; i++) {
+        //     for (int j = 0; j < marks[i].length; j++) {
+        //         System.out.println(marks[i][j]);
+        //     }
+        // }
+
+        // for (int[] mark : marks) {
+        //     for (int mark1 : mark) {
+        //         System.out.println(mark1);
+        //     }
+        // }
+
+        // int[][] marks = new int[3][];
+        // marks[0] = new int[3];
+        // marks[1] = new int[2];
+        // marks[2] = new int[1];
+
+        // marks[0][0] = 10;
+        // marks[0][1] = 20;
+        // marks[0][2] = 30;
+
+        // marks[1][0] = 40;
+        // marks[1][1] = 50;
+
+        // marks[2][0] = 70;
+
+        // System.out.println(marks[0][0]);
+        // System.out.println(marks[0][1]);
+        // System.out.println(marks[0][2]);
+
+        // System.out.println(marks[1][0]);
+        // System.out.println(marks[1][1]);
+
+        // System.out.println(marks[2][0]);
+
+        // for (int i = 0; i < marks.length; i++) {
+        //     for (int j = 0; j < marks[i].length; j++) {
+        //         System.out.println(marks[i][j]);
+        //     }
+        // }
+
+        // for (int[] mark : marks) {
+        //     for (int mark1 : mark) {
+        //         System.out.println(mark1);
+        //     }
+        // }
+
+        // Java automatically initializes the array elements to 0 (Null Value).
+
+        // input element of array
+        // marks[i] = sc.nextInt();
+
+
+
+        // 2D Array - Matrix
+        // type[][] arrayName = new type[row][column];
+        // type[][] arrayName = new type[row][];
+
+        // position - (row, column)
+
+        
+
+
+    }
+}
